5f540512dae3324a2d5170d4ab2199c9fcee934d
Adam R. Smith <adam@adamia.com>
2014-09-19 14:31:28 -0400

    Add comments throughout to help potential contributors.

--- src/plot.jl
+++ src/plot.jl
@@ -12,7 +12,7 @@ end
 
 # See if this float happens to match common uses of constants like n*pi.
 function findsymbolic(num::Real)
-    num == 0 && return false
+    num == 0 && return ""
 
     # Format strings for minus sign, superscript, multiples, and exponents.
     dash(x, super=false) = x >= 0 ? "" : super ? "⁻" : "-"
@@ -27,26 +27,29 @@ function findsymbolic(num::Real)
     isinteger(num/e)        && return "$(multiple(num/e))e"
     isinteger(log(anum))    && return "$(sgn)$(topower("e", log(anum)))"
     isinteger(log10(anum))  && return "$(sgn)$(topower("10", log10(anum)))"
-    return false
+
+    return ""
 end
 
 # Julia somehow doesn't support the crucial "g" printf type.
 # Don't feel like dealing with complex numbers yet.
 function format(num::Real, width::Int, precision::Int=typemax(Int))
-    str = findsymbolic(num)
-    if is(str, false)
+    str = findsymbolic(num)  # Check for a prettier format for num.
+    if length(str) > 0
+        str = lpad(str, width)  # Pretty format found, just fit in the space.
+    else
+        # Attempt multiple rounding levels to fit in available characters.
         precision < width && (num = round(num, precision + 1))
         isint = isinteger(num) || isinteger(round(num, width - 5))
         fmt = isint ? "%$(width)d" : "%$(width).$(width - 3)f"
         str = eval(:(@sprintf $(fmt) $(num)))
-    else
-        str = lpad(str, width)
     end
 
+    # Whatever the output was, number or symbol, truncate if too large.
     if length(str) > width
         str[end] == '.' && return " $(str[1:end - 1])"
         1 < search(str, '.') <= width && return str[1:width]
-        width >= 4 && return str[1:width - 3] * "..."
+        width >= 4 && return str[1:width - 1] * "…"
     end
     str
 end
@@ -57,6 +60,7 @@ function funclabel(f::Function, names=["x"])
         return "$(f.env.name)($(join(names, ", ")))"
     end
 
+    # Julia uses different types for lambdas, named funcs, and inline funcs.
     fsrc = isdefined(f, :code) ? f.code : code_lowered(f, (Number,))[1]
     if isdefined(fsrc, :ast)
         if isa(fsrc.ast, Array)
@@ -81,15 +85,19 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
                  invert::Bool=false, gridlines::Bool=false,
                  scalefunc::Function=identity)
 
+    # The unicode braille chars are conveniently structured to support bitwise
+    # manipulation, so we can just OR specific bits to set that dot to filled.
     grid = fill(char(gridlines ? 0x2812 : 0x2800), cols, rows)
     left, right = sides = ((0, 1, 2, 6), (3, 4, 5, 7))
     function showdot(x, y)  # Assumes x & y are already scaled to the grid.
         invy = (rows - y)*0.9999
         col, col2 = ifloor(x), ifloor(x*2)
         row, row4 = ifloor(invy), ifloor(invy*4)
+        # This does the bitwise OR to fill in a single dot out of the 8.
         grid[col + 1, row + 1] |= 1 << sides[1 + (col2 & 1)][1 + (row4 & 3)]
     end
 
+    # If input is a function, sample it at each row & col to fill xvals & yvals.
     continuous = isa(data, Vector{Function})
     if continuous
         xframes, yframes = cols, rows
@@ -99,6 +107,8 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
         # Assumes f(x) is defined for all x in the given range.
         yvals = Float64[f(x) for x in xvals, f in data]
     else
+        # Unfortunately much of this is redundant with the above code, but a few
+        # things have to be done in a different order, making cleanup tricky.
         xframes, yframes = cols - 1, rows - 1
         xvals, yvals = data
         @assert length(yvals) >= length(xvals) "Different number of x/y points."
@@ -108,11 +118,13 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
         isa(yvals, RealVector) && (yvals = reshape(yvals, length(yvals), 1))
     end
 
+    # Rescaling and setting boundaries must happen after the sampling above.
     xscaled = xscale*(xvals .- start)
     ystart, ystop = minimum(yvals), maximum(yvals)
     yspread = ystop - ystart
     ystep, yscale = yspread/yframes, yframes/yspread
 
+    # Finally, iterate through the grid, scaling & interpolating, to set dots.
     for row in 1:size(yvals, 2)
         rowvals = yvals[:, row]
         yscaled = yscale*(rowvals .- ystart)
@@ -138,8 +150,10 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
         end
     end
 
+    # Exploit the bitwise nature of the braille chars to invert.
     invert && (grid = map(c -> char(c $ 0xFF), grid))
 
+    # Build the plot decorations based on configuration params.
     lines = String[]
     padding = labels ? repeat(" ", margin) : ""
     prefix, suffix = border ? (padding * "⡇", "⢸") : (padding * "", "")
@@ -147,6 +161,7 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
     append!(lines, [prefix * join(grid[:, row], "") * suffix for row in 1:rows])
     border && push!(lines, padding * "⠓" * repeat("⠒", cols) * "⠚")
 
+    # Try pretty hard to fit rounded axis labels into available chars.
     if labels
         yprecision = magnitude(ystart, ystop)
         ystartlabel = format(ystart, margin - 1, yprecision)
@@ -163,6 +178,7 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
         push!(lines, lblrow)
     end
 
+    # The title is mostly useful when plotting repeatedly in a single session.
     if title
         if isa(data, Vector{Function})
             nametag = join([funclabel(f) for f in data], ", ")
@@ -174,6 +190,7 @@ function plot(data::PlotInputs, start::Real=-10, stop::Real=10;
     println(join(lines, '\n'))
 end
 
+# Overloads for a variety of different parameter specifications.
 function plot(data::PlotInputs, rng::Range, etc...; args...)
     stop = isdefined(rng, :stop) ? rng.stop : rng.len
     plot(data, rng.start, stop, etc...; args...)
@@ -196,6 +213,7 @@ end
 function plot(rng::Range, etc...; args...)
     plot(collect(rng))
 end
+# Shorthand for a logarithm plot scale.
 function logplot(etc...; args...)
     plot(etc...; scalefunc=log10, args...)
 end
